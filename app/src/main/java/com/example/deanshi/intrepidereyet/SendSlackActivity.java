328fcffc37bff01b4e630c8375220ab608921bce
Dean Shi <deanshi@Deans-MacBook-Pro.local>
2016-06-09 11:43:59 -0400

    Added OkHttp to post onto Slack

--- app/src/main/java/com/example/deanshi/intrepidereyet/SendSlackActivity.java
+++ app/src/main/java/com/example/deanshi/intrepidereyet/SendSlackActivity.java
@@ -3,6 +3,7 @@ package com.example.deanshi.intrepidereyet;
 import android.app.Activity;
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -38,10 +39,45 @@ public class SendSlackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sendslack);
+        SlackIOTask startSlack = new SlackIOTask();
+        startSlack.execute();
+
+    }
+
+    public class SlackIOTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
 
 
+            MediaType jsonType = MediaType.parse("application/json; charset=utf-8");
+            String jsonString = " { \"text\" : \"dat boi\" }";
+
+            URL url = null;
+            try {
+                url = new URL("https://hooks.slack.com/services/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");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            Timber.d("URL Name Head: %s", url.toString());
+
+            RequestBody slackBody = RequestBody.create(jsonType, jsonString);
+
+            OkHttpClient okClient = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(slackBody)
+                    .build();
+
+            try {
+                Response response = okClient.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
 
+            return null;
+        }
     }
 
 
